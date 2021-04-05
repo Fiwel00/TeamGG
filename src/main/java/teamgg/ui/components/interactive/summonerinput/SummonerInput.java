ad839a0e3f59deefc857afa28de828058a5e014d
Mehdi <mehdi.ayadi@hotmail.de>
2021-04-05 16:01:24 +0200

    fix: summonerinput now shows up properly

--- src/main/java/teamgg/ui/components/interactive/summonerinput/SummonerInput.java
+++ src/main/java/teamgg/ui/components/interactive/summonerinput/SummonerInput.java
@@ -1,16 +1,20 @@
 package teamgg.ui.components.interactive.summonerinput;
 
+import java.awt.Dimension;
+
 import javax.swing.JTextField;
 
 public class SummonerInput extends JTextField{
 
+	private static final int WIDTH = 140;
+	private static final int HEIGHT = 24;
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	
 	public SummonerInput () {
-		setSize(200, 80);
+		setPreferredSize(new Dimension(WIDTH, HEIGHT));
 		
 		getDocument().addDocumentListener(new SummonerInputAL());
 	}
